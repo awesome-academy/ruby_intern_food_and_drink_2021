ebcff9b17d8973bcadcfafc4c8e7865b74f47b03
chungqv-2743 <quan.van.chung@sun-asterisk.com>
2021-11-25 23:56:30 +0700

    Admin All Orders, Filter by Status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     base_title = t "food_drink"
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
+
+  def total_price order
+    sum = 0
+    order.carts.each do |cart|
+      sum += cart.food_price * cart.quantity
+    end
+    number_to_currency(sum, locale: :vi)
+  end
 end
